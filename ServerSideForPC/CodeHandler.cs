a83c815b4800e45dc20ceace594f6340c3b52f4b
Neksus neksus <neksus42@outlook.com>
2025-09-06 01:20:14 +0300

    added slider

--- ServerSideForPC/CodeHandler.cs
+++ ServerSideForPC/CodeHandler.cs
@@ -1,28 +1,119 @@
 ﻿using NAudio.CoreAudioApi;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using System.Globalization;
 using System.Net.Sockets;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
 
 namespace ServerSideForPC
 {
     internal class CodeHandler
     {
+        // === НОВОЕ: сервис громкости + ссылка на последнего клиента ===
+        private static MMDeviceEnumerator _enum;
+        private static MMDevice _device;
+        private static bool _volumeSubscribed;
+        private static TcpClient _lastClient; // единственный телефон
+
+
+        private static DefaultAudioDeviceWatcher _watcher;
+        private static bool _started;
+
+        private static void StartAudioBindingIfNeeded()
+        {
+            if (_started) return;
+
+            _enum = new MMDeviceEnumerator();
+            BindToCurrentDefaultDevice(); // первичная привязка к дефолтному устройству
+
+            // слушаем изменения громкости у текущего устройства
+            _device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotify;
+
+            // слушаем смену дефолтного устройства (Render/Multimedia)
+            _watcher = new DefaultAudioDeviceWatcher(_enum, OnDefaultRenderDeviceChanged);
+            _watcher.Start();
+
+            _started = true;
+        }
+
+        private static void BindToCurrentDefaultDevice()
+        {
+            var newDev = _enum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            if (_device != null && _device.ID == newDev.ID)
+                return;
+
+            if (_device != null)
+                _device.AudioEndpointVolume.OnVolumeNotification -= OnVolumeNotify;
+
+            _device = newDev;
+            _device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotify;
+        }
+
+        private static void OnVolumeNotify(AudioVolumeNotificationData data)
+        {
+            try
+            {
+                if (_lastClient != null)
+                {
+                    var msg = "VolumeSync:" + data.MasterVolume.ToString("0.###", CultureInfo.InvariantCulture);
+                    TcpServer.SendMessage(msg, _lastClient);
+                }
+            }
+            catch { /* игнор */ }
+        }
+
+        private static void OnDefaultRenderDeviceChanged(MMDevice newDefault)
+        {
+            // перепривязка к новому дефолтному устройству
+            if (_device != null)
+                _device.AudioEndpointVolume.OnVolumeNotification -= OnVolumeNotify;
+
+            _device = newDefault;
+            _device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotify;
+
+            // сразу высылаем актуальный уровень нового устройства
+            try
+            {
+                if (_lastClient != null)
+                {
+                    float cur = _device.AudioEndpointVolume.MasterVolumeLevelScalar;
+                    var msg = "VolumeSync:" + cur.ToString("0.###", CultureInfo.InvariantCulture);
+                    TcpServer.SendMessage(msg, _lastClient);
+                }
+            }
+            catch { }
+        }
+
+        private static void EnsureVolumeServiceStarted()
+        {
+            if (_volumeSubscribed) return;
+
+            _enum = new MMDeviceEnumerator();
+            _device = _enum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            // Любое системное изменение громкости → пушим в телефон
+            _device.AudioEndpointVolume.OnVolumeNotification += data =>
+            {
+                TcpServer.SendMessage(
+                    "VolumeSync:" + data.MasterVolume.ToString("0.###", CultureInfo.InvariantCulture),
+                    _lastClient
+                );
+            };
+
+            _volumeSubscribed = true;
+        }
         static public async void CodeHandlerFunc(string jsonstring, TcpClient tcpClient)
         {
+            _lastClient = tcpClient;
 
+            StartAudioBindingIfNeeded();
 
             string[] subarr = jsonstring.Split(':', 2);
             string Code = subarr[0].Trim();
-        
+            EnsureVolumeServiceStarted();
 
             Console.WriteLine($"Код задачи: {Code}");
            
@@ -62,6 +153,8 @@ namespace ServerSideForPC
                         string deviceId = devices[Convert.ToInt32(subarr[1])].ID;
 
                         AudioDeviceManager.SetDefaultAudioPlaybackDevice(deviceId);
+
+
                         Console.WriteLine("Устройство успешно установлено по умолчанию.");
                         break;
                     }
@@ -77,7 +170,38 @@ namespace ServerSideForPC
                         await AudioDeviceManager.WaitAndSetDefaultAsync("TV", TimeSpan.FromSeconds(10));
                         break;
                     }
-                
+                case "GetVolume":
+                    {
+                        BindToCurrentDefaultDevice();
+                        float cur = _device.AudioEndpointVolume.MasterVolumeLevelScalar;
+                        TcpServer.SendMessage(
+                            "VolumeSync:" + cur.ToString("0.###", CultureInfo.InvariantCulture),
+                            tcpClient
+                        );
+                        break;
+
+                    }
+
+                case "SetVolume":
+                    {
+                        BindToCurrentDefaultDevice();
+
+                        if (subarr.Length > 1 &&
+                            float.TryParse(subarr[1], NumberStyles.Float, CultureInfo.InvariantCulture, out float v))
+                        {
+                            if (v > 1f) v /= 100f;            // поддержка процентов
+                            v = Math.Clamp(v, 0f, 1f);
+
+                            _device.AudioEndpointVolume.MasterVolumeLevelScalar = v;
+                            float cur = _device.AudioEndpointVolume.MasterVolumeLevelScalar;
+                            TcpServer.SendMessage(
+                                "VolumeSync:" + cur.ToString("0.###", CultureInfo.InvariantCulture),
+                                tcpClient
+                            );
+                            break;
+                        }
+                        break;
+                    }
 
             }
         }
@@ -95,6 +219,8 @@ namespace ServerSideForPC
 
 
     }
+
+
 
 }
 
---
+++ ServerSideForPC/DefaultAudioDeviceWatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using PropertyKeyNAudio = NAudio.CoreAudioApi.PropertyKey;
+
+public sealed class DefaultAudioDeviceWatcher : IMMNotificationClient, IDisposable
+{
+    private readonly MMDeviceEnumerator _enumerator;
+    private readonly Action<MMDevice> _onDefaultRenderChanged;
+    private bool _registered;
+
+    public DefaultAudioDeviceWatcher(MMDeviceEnumerator enumerator, Action<MMDevice> onDefaultRenderChanged)
+    {
+        _enumerator = enumerator ?? throw new ArgumentNullException(nameof(enumerator));
+        _onDefaultRenderChanged = onDefaultRenderChanged ?? throw new ArgumentNullException(nameof(onDefaultRenderChanged));
+    }
+
+    public void Start() { if (_registered) return; _enumerator.RegisterEndpointNotificationCallback(this); _registered = true; }
+    public void Stop() { if (!_registered) return; _enumerator.UnregisterEndpointNotificationCallback(this); _registered = false; }
+    public void Dispose() => Stop();
+
+    public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+    {
+        if (flow == DataFlow.Render && role == Role.Multimedia)
+        {
+            try { var dev = _enumerator.GetDevice(defaultDeviceId); _onDefaultRenderChanged(dev); } catch { }
+        }
+    }
+
+    public void OnDeviceAdded(string pwstrDeviceId) { }
+    public void OnDeviceRemoved(string deviceId) { }
+    public void OnDeviceStateChanged(string deviceId, DeviceState newState) { }
+    public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKeyNAudio key) { }
+}
